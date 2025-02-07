5c4317eb89ae05502467add550c57645fe216850
HatoChanChikako <c.hatopoppo@gmail.com>
2025-02-07 14:42:51 +0900

    Initial frontend commit

---
+++ src/app/customers/fetchCustomers.js
@@ -0,0 +1,10 @@
+export default async function fetchCustomers() {
+  console.log("process.env.API_ENDPOINT: ",process.env.API_ENDPOINT);
+  const res = await fetch(process.env.API_ENDPOINT + "/allcustomers", {
+    cache: "no-cache",
+  });
+  if (!res.ok) {
+    throw new Error("Failed to fetch customers");
+  }
+  return res.json();
+}
